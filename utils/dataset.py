0964c11e521918cab1de62e2048c60708f8fe0c3
Vardan Narula <vardannarula@gmail.com>
2024-05-17 06:23:21 +0200

    conflict fix

--- utils/dataset.py
+++ utils/dataset.py
@@ -155,10 +155,11 @@ def build_dataset(hf_dataset, tokenizer):
         tokenize_fn = tokenize_sib200
 
     elif hf_dataset == WIKIANN:
-        raise Exception(f"Use the build_dataset_wikiann func for {hf_dataset}")
+        #leaving this here just cause its creating a conflict, but use the build_datatset_wikiann() func for wikiann
+        dataset = load_dataset(WIKIANN, "en")
+        tokenize_fn = tokenize_wikiann
 
     elif hf_dataset == MARC:
-        # TODO: load the correct data language subset
         dataset = load_dataset(MARC)
         tokenize_fn = tokenize_marc
     else:
