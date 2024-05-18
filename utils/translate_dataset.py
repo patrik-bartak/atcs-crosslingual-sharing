fe0ad0dfef6802e21523a786924289c06967ec96
Gregory Go <gregorygocc@gmail.com>
2024-05-18 14:48:56 +0200

    fix: update translation script

--- utils/translate_dataset.py
+++ utils/translate_dataset.py
@@ -1,16 +1,22 @@
+# This script is used to translate instances for languages that are not in XNLI
+# First run "pip install argostranslate"
+
 import argostranslate.package
 import argostranslate.translate
 from datasets import load_dataset
-
-from utils import XNLI
+from constants import XNLI
 
 
 def translate_xnli(to_lang, from_lang="en"):
     dataset = load_dataset(XNLI, from_lang)
-    # Only translate test and validation sets
+    # Only translate the validation set
     del dataset["train"]
+    del dataset["test"]
+
+    # Save the original data for conversion
     dataset.save_to_disk("data/en_xnli_test_val")
 
+    # Get the translation packages
     argostranslate.package.update_package_index()
     available_packages = argostranslate.package.get_available_packages()
     package_to_install = next(
@@ -22,18 +28,31 @@ def translate_xnli(to_lang, from_lang="en"):
     argostranslate.package.install_from_path(package_to_install.download())
 
     def translate_fn(row):
+
+        # print(f"Before: {row}")
+
         row["premise"] = argostranslate.translate.translate(
             row["premise"], from_lang, to_lang
         )
+
         row["hypothesis"] = argostranslate.translate.translate(
             row["hypothesis"], from_lang, to_lang
         )
 
-    return dataset.map(translate_fn)
+        return row
+
+        # print(f"After: {row}")
+
+    dataset = dataset.map(translate_fn)
+
+    return dataset
 
 
-langs_to_translate = ["cs", "id"]
+# Specify the languages here
+langs_to_translate = ["nl", "id"]
 
 for lang in langs_to_translate:
+
     print(f"Translating {lang}")
-    translate_xnli(lang).save_to_disk(f"data/{lang}_xnli_test_val")
+    data = translate_xnli(lang)
+    data.save_to_disk(f"data/{lang}_xnli_test_val")
