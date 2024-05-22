d93334efe27a539dbee86c9ed16d074a7e8167f6
ToyeshC <toyesh.c@gmail.com>
2024-05-22 14:36:47 +0200

    job files updated

--- job-files/toxi-finetune-trial.sh
+++ job-files/toxi-finetune-trial.sh
@@ -1,6 +1,7 @@
 python finetune.py \
-    --dataset=xnli \
+    --dataset=FredZhang7/toxi-text-3M \
     --batch_size=32 \
     --epochs=1 \
     --max_steps=20000 \
-    --cuda
+    --cuda \
+    --test_run
---
+++ job-files/toxi-finetune.sh
@@ -0,0 +1,6 @@
+python finetune.py \
+    --dataset=FredZhang7/toxi-text-3M \
+    --batch_size=16 \
+    --epochs=5 \
+    --max_steps=10000 \
+    --cuda
